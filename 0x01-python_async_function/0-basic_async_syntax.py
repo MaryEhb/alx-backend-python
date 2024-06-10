6aef00954ccae633067ab59dc069e7860c34f555
Mariem Ehab <mariem.ehab234@gmail.com>
2024-06-10 22:31:02 +0000

    Task 0 adding types

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     '''takes in an integer that waits for a random delay between 0
     and that int and eventually returns it'''
     random_num = random.uniform(1, max_delay)
