10c594768c4b6d8b3dc44a5fa7a47d4538fca267
Mariem <mariem.ehab234@gmail.com>
2024-07-28 12:10:23 +0000

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''0. Parameterize a unit test'''
+'''unit tests for Utils.py'''
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -16,3 +16,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, input_list, path, return_val):
         '''test that the method returns what it is supposed to.'''
         self.assertEqual(access_nested_map(input_list, path), return_val)
+
+    @parameterized.expand([
+        [{}, ("a",), 'a'],
+        [{"a": 1}, ("a", "b"), 'b']
+    ])
+    def test_access_nested_map_exception(self, input_list, path, key):
+        '''test that a KeyError is raised'''
+        with self.assertRaises(KeyError) as raises:
+            access_nested_map(input_list, path)
+        self.assertEqual(raises.exception.args[0], key)
+
+
+if __name__ == '__main__':
+    unittest.main()
