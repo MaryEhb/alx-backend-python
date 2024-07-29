feb176277c31d438519dd4e033379926100ef48d
Mariem <mariem.ehab234@gmail.com>
2024-07-29 08:55:45 +0000

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''unittests for client.py'''
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -22,6 +23,16 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self):
+        '''method to unit-test GithubOrgClient._public_repos_url'''
+        with patch('client.GithubOrgClient.org',
+                new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "something"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
 
 
 if __name__ == '__main__':
